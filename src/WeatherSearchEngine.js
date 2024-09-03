73bfec92d767c4e5644e28cd659adde3574acbfa
Muri <mcsanchezlaguna@gmail.com>
2024-09-03 21:03:50 +0200

    API Integration (part 1)

--- src/WeatherSearchEngine.js
+++ src/WeatherSearchEngine.js
@@ -1,48 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherSearchEngine.css";
-
+import axios from "axios";
 export default function WeatherSearchEngine() {
-  return (
-    <div className="WeatherSearchEngine">
-      <form>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="formSearch"
-          autoFocus="on"
-        />
-        <input type="submit" value="Search" className="formSubmit" />
-      </form>
-      <h3>Ney York, USA</h3>
-      <ul>
-        <li>Wednesday September 3, 2024</li>
-        <li>Sunny</li>
-      </ul>
-      <div className="row">
-        <div className="col-6 temperature-container">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="sunny"
-            className="mainImage"
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+
+  function handleResponse(response) {
+    setReady(true);
+
+    setWeatherData({
+      temperature: response.data.daily[0].temperature.day,
+      city: response.data.city,
+      country: response.data.country,
+      description: response.data.daily[0].condition.description,
+      humidity: response.data.daily[0].temperature.humidity,
+      wind: response.data.daily[0].wind.speed,
+      iconUrl: response.data.daily[0].condition.icon_url,
+      icon: response.data.daily[0].condition.icon,
+    });
+  }
+  if (ready) {
+    return (
+      <div className="WeatherSearchEngine">
+        <form>
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            className="formSearch"
+            autoFocus="on"
           />
-          <span className="temperature">
-            24<small>°C</small>
-          </span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>
-              Precipitation: <span>0%</span>
-            </li>
-            <li>
-              Humidity: <span>81%</span>
-            </li>
-            <li>
-              Wind: <span>13km/h</span>
-            </li>
-          </ul>
+          <input type="submit" value="Search" className="formSubmit" />
+        </form>
+        <h3>
+          {weatherData.city}, {weatherData.country}
+        </h3>
+        <ul>
+          <li>Wednesday September 3, 2024</li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6 temperature-container">
+            <img
+              src={weatherData.icon_url}
+              alt={weatherData.icon}
+              className="mainImage"
+            />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+              <small>°C</small>
+            </span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>
+                Humidity: <span>{weatherData.humidity}%</span>
+              </li>
+              <li>
+                Wind: <span>{weatherData.wind}km/h</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let query = "Valencia";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${query}&key=73050fa355794447f81ab5349190dotd&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading";
+  }
 }
