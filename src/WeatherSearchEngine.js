85c7b701b45a7802528d7d8466c9cf34ee768c54
Muri <mcsanchezlaguna@gmail.com>
2024-09-03 18:37:38 +0200

    Added the basic HTML

--- src/WeatherSearchEngine.js
+++ src/WeatherSearchEngine.js
@@ -1,5 +1,38 @@
 import React from "react";
+import "./WeatherSearchEngine.css";
 
 export default function WeatherSearchEngine() {
-  return <div>insert here the actual weather app</div>;
+  return (
+    <div className="WeatherSearchEngine">
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          className="formSearch"
+        />
+        <input type="submit" value="Search" className="formSubmit" />
+      </form>
+      <h3>Ney York, USA</h3>
+      <ul>
+        <li>Wednesday September 3, 2024</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="sunny"
+          />
+          <h2>24ºC</h2>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 81%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
