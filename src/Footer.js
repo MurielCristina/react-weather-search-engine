545b4af54548220f9915654ae7462703ff4b2143
Muri <mcsanchezlaguna@gmail.com>
2024-09-03 12:18:07 +0200

    Added the footers url's

--- src/Footer.js
+++ src/Footer.js
@@ -6,8 +6,30 @@ export default function Footer() {
   return (
     <div className="Footer">
       <p>
-        This apps was created in React by Muri, it's open sourced at GitHub and
-        hosted at Netlify
+        This apps was created in React by{" "}
+        <a
+          href="https://muri-portfolio.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Muri
+        </a>
+        , it's{" "}
+        <a
+          href="https://github.com/MurielCristina/react-weather-search-engine"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced at GitHub
+        </a>{" "}
+        and hosted at{" "}
+        <a
+          href="https://app.netlify.com/sites/calm-mousse-9a4410/overview"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
       </p>
     </div>
   );
