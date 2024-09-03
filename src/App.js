31a0e1f55bbc844ca1208a8ee7383691740953e6
Muri <mcsanchezlaguna@gmail.com>
2024-09-03 12:12:17 +0200

    Added a footer as a new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 
+import Footer from "./Footer";
+
 export default function App() {
   return (
     <div className="App">
       <h1>REACT WEATHER SEARCH ENGINE</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This apps was created in React by Muri, it's open sourced at GitHub and
+        hosted at Netlify
+      </p>
+    </div>
+  );
+}
