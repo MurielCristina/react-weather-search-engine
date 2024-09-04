4e01146f0f859a9d1c8059efce862e5ff58373a5
Muri <mcsanchezlaguna@gmail.com>
2024-09-04 18:16:20 +0200

    looping

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,15 +4,18 @@ import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-  console.log(props.data);
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">
-          <WeatherForecastDay forecastDay={props.data[0]} />
-        </div>
-        {/*delete from here */}
-        {/*until here */}
+        {props.data.map((dailyForecast, index) => {
+          if (index < 5) {
+            return (
+              <div className="col" key={index}>
+                <WeatherForecastDay forecastDay={dailyForecast} />
+              </div>
+            );
+          }
+        })}
       </div>
     </div>
   );
