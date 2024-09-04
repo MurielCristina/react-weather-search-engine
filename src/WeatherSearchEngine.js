497eb775a62e851bf79b2a8b6a95e6ae3aaec4a1
Muri <mcsanchezlaguna@gmail.com>
2024-09-04 13:54:00 +0200

    fix missing component

--- src/WeatherSearchEngine.js
+++ src/WeatherSearchEngine.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
 
 import "./WeatherSearchEngine.css";
 
